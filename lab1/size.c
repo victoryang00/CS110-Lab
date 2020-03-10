bf62180e8550aa095e2284606e0de124be40296f
root <root@H4CK5T4T1ON.localdomain>
2020-03-10 12:34:02 +0800

    fix

--- lab1/size.c
+++ lab1/size.c
@@ -1,41 +1,26 @@
- #include<stdlib.h>
-#include<stdio.h>
-#include<time.h>
-/* There is some amount of overlap with <sys/types.h> as known by inet code */
-#ifndef __int8_t_defined
-# define __int8_t_defined
-typedef signed char        int8_t;
-typedef short int          int16_t;
-typedef int                int32_t;
-# if __WORDSIZE == 64
-typedef long int           int64_t;
-# else
-__extension__
-typedef long long int      int64_t;
-# endif
-#endif
-
-int main()
-{
-printf("Size of char: %d\n",sizeof(char));
-printf("Size of short: %d\n",sizeof(short));
-printf("Size of short int: %d\n",sizeof(short int));
-printf("Size of int: %d\n",sizeof(int));
-printf("Size of long int: %d\n",sizeof(long int));
-printf("Size of unsigned int: %d\n",sizeof(unsigned int));
-printf("Size of void *: %d\n",sizeof(void *));
-printf("Size of size_t: %d\n",sizeof(size_t));
-printf("Size of float: %d\n",sizeof(float));
-printf("Size of double: %d\n",sizeof(double));
-printf("Size of int8_t: %d\n",sizeof(int8_t));
-printf("Size of int16_t: %d\n",sizeof(int16_t));
-printf("Size of int64_t: %d\n",sizeof(int64_t));
-printf("Size of time_t: %d\n",sizeof(time_t));
-printf("Size of clock_t: %d\n",sizeof(clock_t));
-printf("Size of struct tm: %d\n",sizeof(struct tm));
-printf("Size of NULL: %d\n",sizeof(NULL));
-return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <inttypes.h>
+#define sz(type) printf("Size of %s: %d\n", #type, (int)sizeof(type));
+int main() {
+    sz(char);
+    sz(short);
+    sz(short int);
+    sz(int);
+    sz(long int);
+    sz(unsigned int);
+    sz(void *);
+    sz(size_t);
+    sz(float);
+    sz(double);
+    sz(int8_t);
+    sz(int16_t);
+    sz(int32_t);
+    sz(int64_t);
+    sz(time_t);
+    sz(clock_t);
+    sz(struct tm);
+    sz(NULL);
+    return 0;
 }
-
-
-
